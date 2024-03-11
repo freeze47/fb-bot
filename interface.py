96cc23a3a6782eb4d5746392243eed3815915fa1
freeze47 <79481427+freeze47@users.noreply.github.com>
2024-03-11 23:25:16 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ import json
 
 def get_page_by_interface():
     x = requests.get(
-        'https://fb-friends-23f0d-default-rtdb.europe-west1.firebasedatabase.app/friends.json')
+        '')
     request_data = x.content
     data = json.loads(request_data)
     data_array = list(data.keys())
